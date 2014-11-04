46c9db3b5685008eadf484b3652abdc39dd34c3b
K4zuki <k.yamamoto.08136891@gmail.com>
2014-11-04 20:34:17 +0900

    added monthname.py
    added monthname.py

---
+++ monthname.py
@@ -0,0 +1,37 @@
+import datetime
+
+def monthname():
+    todaydetail = datetime.datetime.today()
+    mnum=todaydetail.strftime("%m")
+    mname=""
+    if mnum == "01":
+        mname="Jan"
+    elif mnum == "02":
+        mname="Feb"
+    elif mnum == "03":
+        mname="Mar"
+    elif mnum == "04":
+        mname="Apr"
+    elif mnum == "05":
+        mname="May"
+    elif mnum == "06":
+        mname="Jun"
+    elif mnum == "07":
+        mname="Jul"
+    elif mnum == "08":
+        mname="Aug"
+    elif mnum == "09":
+        mname="Sep"
+    elif mnum == "10":
+        mname="Oct"
+    elif mnum == "11":
+        mname="Nov"
+    elif mnum == "12":
+        mname="Dec"
+        
+    basename=todaydetail.strftime("%Y.%m")+mname+todaydetail.strftime(".%d")
+    return basename
+    
+if __name__ == "__main__":
+    print "this file returns directory name baseed on date of today."
+    print monthname()
