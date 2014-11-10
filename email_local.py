d2c267da84d08ab9e5b3e32573b27d910d8bfca4
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2014-11-10 16:57:37 +0900

    [ RENAME ] email.py renamed to email_local.py
    email.py renamed to email_local.py

--- email_local.py
+++ email_local.py
@@ -3,9 +3,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 msg = MIMEMultipart()
-sender = 'user@example.com'
+sender = 'user@example.com' #will be changed
 subject = "[ LAB ]tempctrl finished"
-body = "tempchamber program finished on [machinename]\n"
+body = "tempchamber program finished on [machinename] at [finished time].\n"
 body +="outputs are [outputfilename] and [logfilename]"
 
 msg['From'] = sender
@@ -15,7 +15,7 @@ msg.attach(MIMEText(body, 'plain'))
 text=msg.as_string()
 #print text
 # Send the message via our SMTP server
-s = smtplib.SMTP('smtp.server.com')
-s.sendmail(sender,address_book, text)
+s = smtplib.SMTP('smtp.server.com') #will be changed
+s.sendmail(sender,sender, text)
 s.quit()
 
