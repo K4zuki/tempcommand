597f5b7581518bb9404a4c4ddaaab85e2521a258
K4zuki <k.yamamoto.08136891@gmail.com>
2014-11-11 15:24:42 +0900

    path change
    spec file path change

--- Makefile.bat
+++ Makefile.bat
@@ -1,1 +1,1 @@
-C:\Python27\python.exe C:\Python27\Scripts\pyinstaller-script.py C:\Users\kyamamot\Documents\GitHub\tempcommand\TCtester.spec
+C:\Python27\python.exe C:\Python27\Scripts\pyinstaller-script.py TCtester.spec
