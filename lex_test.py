4aad4398e9908bf902c11114c30cd4350159352a
K4zuki <k.yamamoto.08136891@gmail.com>
2016-06-21 19:04:22 +0900

    add License; comment out commands

--- lex_test.py
+++ lex_test.py
@@ -1,4 +1,23 @@
+
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Kazuki Yamamoto
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in the
+# Software without restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the
+# following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import shlex
 import pprint
@@ -9,7 +28,7 @@ chan(105,110);
 base(90); ubase(0,1: 90);
 reg(00 : ff);
 ureg(0,1: 00 : ff);
-TEMP(25);
+# TEMP(25);
 base(D2); ubase(0,1: D2);
     reg(41:00); for(reg:42:02,B0,A9,8A,A7,A8,B1){}
     ureg(0,1:41:00); for(ureg:0,1:42:02,B0,A9,8A,A7,A8,B1){}
@@ -18,17 +37,19 @@ base(D2); ubase(0,1: D2);
     ureg(0,1:47:01);
 for(TEMP:  -40,0,100 )
 {
-    dely(1); sample();
+    dely(1);
+    # sample();
     for(reg: 00: 00,02,04)
     {
-        sample();
+        # sample();
     }
     for(reg: 02: 0c,0e)
     {
         for(reg: 01,0A: 00,02,04)
         {
-            sample();
-            sample();
+
+            # sample();
+            # sample();
         }
     }
     suspend;
@@ -117,19 +138,18 @@ def serialize_loop(arg):
     return _stack
     # callback[ cmd ]( (None, 0, _cmd, _arg ))
 
-command = ["",""]
-callback = {
-    "NOP":nop,
-    "REG":reg,
-    "UREG":reg,
-    "CHAN":reg,
-    "BASE":reg,
-    "UBASE":reg,
-    "TEMP":reg,
-    "DELY":reg,
-    "SAMPLE":reg,
-    "SUSPEND":suspend,
-}
+# callback = {
+#     "NOP":nop,
+#     "REG":reg,
+#     "UREG":reg,
+#     "CHAN":reg,
+#     "BASE":reg,
+#     "UBASE":reg,
+#     "TEMP":reg,
+#     "DELY":reg,
+#     "SAMPLE":reg,
+#     "SUSPEND":suspend,
+# }
 
 _tok = []
 __tok = []
@@ -145,7 +165,7 @@ while(True):
             __tok.append([_tok[0],"".join(_tok[1:])])
             _tok = []
         if(tok == "}"):
-            _f = []
+            # _f = []
             _for = []
             # _for.append([_tok[0],"".join(_tok[1:])])
             while not (__tok[-1][0] == "FOR"):
@@ -158,9 +178,9 @@ while(True):
             __tok.extend( serialize_loop( (_for, 0, _f[0], _f[1]) ) )
             # print pprint.pprint(__tok)
             _tok = []
-# print pprint.pprint(__tok)
+print pprint.pprint(__tok)
 
-for i, command in enumerate(__tok):
-    cmd = command[0]
-    arg = command[1]
-    callback[ cmd ]( arg )
+# for i, command in enumerate(__tok):
+#     cmd = command[0]
+#     arg = command[1]
+#     callback[ cmd ]( arg )
