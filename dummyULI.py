b17da9615888d0b0cb646f663eb13ce9b8166510
K4zuki <k.yamamoto.08136891@gmail.com>
2016-06-22 15:02:02 +0900

    line ending

--- dummyULI.py
+++ dummyULI.py
@@ -1,17 +1,16 @@
-#-*- coding: utf-8 -*- 
+#-*- coding: utf-8 -*-
 #!/usr/bin/env python
 
 ## dummy class to replace ULI class
 class I2C(object):
     def __init__(self,module,slave,channel):
         pass
-        
+
     def searchI2CDev(self,begin,end):
         pass
-        
+
     def write_register(self, reg, data):
         pass
-        
+
 #    def I2C(self,module,slave,channel):
 #        pass
-        
