949c64f0ea8a17b1a6fae9081166b75a43540a72
K4zuki <k.yamamoto.08136891@gmail.com>
2016-07-05 11:01:48 +0900

    remove testing script; add parse()

--- lex_test.py
+++ lex_test.py
@@ -22,61 +22,6 @@
 import shlex
 import pprint
 
-tokens = shlex.shlex(
-"""
-chan(105,110);
-base(90); ubase(0,1: 90);
-reg(00 : ff);
-ureg(0,1: 00 : ff);
-# TEMP(25);
-base(D2); ubase(0,1: D2);
-    reg(41:00); for(reg:42:02,B0,A9,8A,A7,A8,B1){}
-    ureg(0,1:41:00); for(ureg:0,1:42:02,B0,A9,8A,A7,A8,B1){}
-    # ureg(1:41:00); for(ureg:1:42:02,B0,A9,8A,A7,A8,B1){}
-    reg(47:01);
-    ureg(0,1:47:01);
-for(TEMP:  -40,0,100 )
-{
-    dely(1);
-    # sample();
-    for(reg: 00: 00,02,04)
-    {
-        # sample();
-    }
-    for(reg: 02: 0c,0e)
-    {
-        for(reg: 01,0A: 00,02,04)
-        {
-
-            # sample();
-            # sample();
-        }
-    }
-    suspend;
-}
-suspend;
-EOF;
-sample
-""".upper()
-)
-
-def nop(arg):
-    arg = arg.strip("{}();")
-    arg = arg.split(":")
-    print arg
-    # return idx
-
-def reg(arg):
-    arg = arg.strip("{}();")
-    arg = arg.split(":")
-    print arg
-
-def suspend(arg):
-    arg = arg.strip("{}();")
-    arg = arg.split(":")
-    print(" SUSPEND: press return to continue ".center(80, "#"))
-    raw_input()
-
 def serialize_loop(arg):
     __tok,idx,cmd,arg = arg
     arg = arg.strip("{}();")
@@ -138,49 +83,68 @@ def serialize_loop(arg):
     return _stack
     # callback[ cmd ]( (None, 0, _cmd, _arg ))
 
-# callback = {
-#     "NOP":nop,
-#     "REG":reg,
-#     "UREG":reg,
-#     "CHAN":reg,
-#     "BASE":reg,
-#     "UBASE":reg,
-#     "TEMP":reg,
-#     "DELY":reg,
-#     "SAMPLE":reg,
-#     "SUSPEND":suspend,
-# }
+def nop(arg):
+    arg = arg.strip("{}();")
+    arg = arg.split(":")
+    print arg
+    # return idx
 
-_tok = []
-__tok = []
-_for = []
-while(True):
-    tok = tokens.get_token()
-    if (not tok) or (tok=="EOF"):
-        break
-    else:
-        # print tok
-        _tok.append(tok)
-        if(tok=="{" or tok==";"):
-            __tok.append([_tok[0],"".join(_tok[1:])])
-            _tok = []
-        if(tok == "}"):
-            # _f = []
-            _for = []
-            # _for.append([_tok[0],"".join(_tok[1:])])
-            while not (__tok[-1][0] == "FOR"):
-                # print __tok[-1]
-                _for.append(__tok.pop())
-            _f = __tok.pop()
-            # print pprint.pprint(_for)
-            # print _f
-            _for.reverse()
-            __tok.extend( serialize_loop( (_for, 0, _f[0], _f[1]) ) )
-            # print pprint.pprint(__tok)
-            _tok = []
-print pprint.pprint(__tok)
+def reg(arg):
+    arg = arg.strip("{}();")
+    arg = arg.split(":")
+    print arg
 
-# for i, command in enumerate(__tok):
-#     cmd = command[0]
-#     arg = command[1]
-#     callback[ cmd ]( arg )
+def suspend(arg):
+    arg = arg.strip("{}();")
+    arg = arg.split(":")
+    print(" SUSPEND: press return to continue ".center(80, "#"))
+    raw_input()
+
+callback = {
+   "NOP":nop,
+   "REG":reg,
+   "UREG":reg,
+   "CHAN":reg,
+   "BASE":reg,
+   "UBASE":reg,
+   "TEMP":reg,
+   "DELY":reg,
+   "SAMPLE":reg,
+   "SUSPEND":suspend,
+}
+
+def parse(script = "eof"):
+    _tok = []
+    __tok = []
+    _for = []
+    tokens = shlex.shlex(script.upper())
+    while(True):
+        tok = tokens.get_token()
+        if (not tok) or (tok=="EOF"):
+            break
+        else:
+            # print tok
+            _tok.append(tok)
+            if(tok=="{" or tok==";"):
+                __tok.append([_tok[0],"".join(_tok[1:])])
+                _tok = []
+            if(tok == "}"):
+                # _f = []
+                _for = []
+                # _for.append([_tok[0],"".join(_tok[1:])])
+                while not (__tok[-1][0] == "FOR"):
+                    # print __tok[-1]
+                    _for.append(__tok.pop())
+                _f = __tok.pop()
+                # print pprint.pprint(_for)
+                # print _f
+                _for.reverse()
+                __tok.extend( serialize_loop( (_for, 0, _f[0], _f[1]) ) )
+                # print pprint.pprint(__tok)
+                _tok = []
+    # print pprint.pprint(__tok)
+
+    for i, command in enumerate(__tok):
+        cmd = command[0]
+        arg = command[1]
+        callback[ cmd ]( arg )
